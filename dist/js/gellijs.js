4661335941a567cda7c1465739d29ef804d20ca3
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-08-26 01:35:59 -0400

    added the fade in css as well as js function

---
+++ dist/js/gellijs.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var dinnerNav = document.querySelector(".dinner");
+
+dinnerNav.addEventListener("click", function () {
+    dinnerNav.classList.toggle("active");
+});
+//# sourceMappingURL=gellijs.js.map
---
+++ src/js/gellijs.js
@@ -0,0 +1,5 @@
+let dinnerNav = document.querySelector(`.dinner`)
+
+dinnerNav.addEventListener(`click`, function(){
+    dinnerNav.classList.toggle(`active`)
+})
