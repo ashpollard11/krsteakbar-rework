b2e9fe09ce05df9a041af2d562a87fc00986943a
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-08-27 00:14:53 -0400

    added blue coloring to the reservations

--- dist/js/gellijs.js
+++ dist/js/gellijs.js
@@ -1,9 +1,15 @@
-'use strict';
+"use strict";
 
-var dinnerNav = document.querySelector('.dinner');
+var dinnerNav = document.querySelector(".dinner");
+var navLI = document.querySelectorAll("nav li");
+var phoneNumberDiv = document.querySelector(".phone-number");
+var reservationsDiv = document.querySelector(".reservations");
 
-dinnerNav.addEventListener('click', function () {
-    document.body.classList.toggle('dinnerBG');
-    dinnerNav.classList.toggle('dinner-highlight');
+dinnerNav.addEventListener("click", function () {
+    document.body.classList.toggle("dinnerBG");
+    phoneNumberDiv.classList.toggle("dinnerBodyCopy");
+    reservationsDiv.classList.toggle("dinnerBodyCopy");
+    // bodyCopy.classList.toggle(`dinnerBodyCopy`)
+    dinnerNav.classList.toggle("dinner-highlight");
 });
 //# sourceMappingURL=gellijs.js.map
--- src/js/gellijs.js
+++ src/js/gellijs.js
@@ -1,7 +1,13 @@
 let dinnerNav = document.querySelector(`.dinner`)
+let navLI = document.querySelectorAll(`nav li`)
+let phoneNumberDiv = document.querySelector(".phone-number")
+let reservationsDiv = document.querySelector(".reservations")
 
 
 dinnerNav.addEventListener(`click`, function(){
-    document.body.classList.toggle('dinnerBG')
-    dinnerNav.classList.toggle('dinner-highlight')
+    document.body.classList.toggle(`dinnerBG`)
+    phoneNumberDiv.classList.toggle(`dinnerBodyCopy`)
+    reservationsDiv.classList.toggle(`dinnerBodyCopy`)
+    // bodyCopy.classList.toggle(`dinnerBodyCopy`)
+    dinnerNav.classList.toggle(`dinner-highlight`)
 })
