ef5159e449172ae4461038c356775ae5c08d1dc2
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-08-27 02:49:10 -0400

    panel somewhat working

--- dist/js/gellijs.js
+++ dist/js/gellijs.js
@@ -11,9 +11,19 @@ dinnerNav.addEventListener("click", function () {
     phoneNumberDiv.classList.toggle("dinnerBodyCopy");
     reservationsDiv.classList.toggle("dinner-highlight");
     krfooterDiv.classList.toggle("dinnerBodyCopy");
+    var panel = document.querySelector(".panel");
+
     navLIs.forEach(function (navLi) {
         navLi.classList.toggle("dinner-highlight-others");
     });
     dinnerNav.classList.toggle("dinner-highlight");
+
+    if (panel.classList.contains("close")) {
+        panel.classList.add("open");
+        panel.classList.remove("close");
+    } else {
+        panel.classList.add("close");
+        panel.classList.remove("add");
+    }
 });
 //# sourceMappingURL=gellijs.js.map
--- src/js/gellijs.js
+++ src/js/gellijs.js
@@ -10,8 +10,18 @@ dinnerNav.addEventListener(`click`, function(){
     phoneNumberDiv.classList.toggle(`dinnerBodyCopy`)
     reservationsDiv.classList.toggle(`dinner-highlight`)
     krfooterDiv.classList.toggle(`dinnerBodyCopy`)
+    const panel = document.querySelector(".panel")
+
     navLIs.forEach(function(navLi){
         navLi.classList.toggle(`dinner-highlight-others`)
     })
     dinnerNav.classList.toggle(`dinner-highlight`)
+
+    if (panel.classList.contains("close")){
+        panel.classList.add("open");
+        panel.classList.remove("close");
+    } else {
+        panel.classList.add("close");
+        panel.classList.remove("add");
+    }
 })
