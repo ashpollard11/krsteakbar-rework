a911641ac9a5531d509069734ec8adf68076dff6
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-08-27 00:54:40 -0400

    styling for dinner complete

--- dist/js/gellijs.js
+++ dist/js/gellijs.js
@@ -1,15 +1,19 @@
 "use strict";
 
 var dinnerNav = document.querySelector(".dinner");
-var navLI = document.querySelectorAll("nav li");
+var navLIs = document.querySelectorAll("nav a");
 var phoneNumberDiv = document.querySelector(".phone-number");
 var reservationsDiv = document.querySelector(".reservations");
+var krfooterDiv = document.querySelector(".kr-footer");
 
 dinnerNav.addEventListener("click", function () {
     document.body.classList.toggle("dinnerBG");
     phoneNumberDiv.classList.toggle("dinnerBodyCopy");
     reservationsDiv.classList.toggle("dinnerBodyCopy");
-    // bodyCopy.classList.toggle(`dinnerBodyCopy`)
+    krfooterDiv.classList.toggle("dinnerBodyCopy");
+    navLIs.forEach(function (navLi) {
+        navLi.classList.toggle("dinner-highlight-others");
+    });
     dinnerNav.classList.toggle("dinner-highlight");
 });
 //# sourceMappingURL=gellijs.js.map
--- src/js/gellijs.js
+++ src/js/gellijs.js
@@ -1,13 +1,17 @@
 let dinnerNav = document.querySelector(`.dinner`)
-let navLI = document.querySelectorAll(`nav li`)
+let navLIs = document.querySelectorAll(`nav a`)
 let phoneNumberDiv = document.querySelector(".phone-number")
 let reservationsDiv = document.querySelector(".reservations")
+let krfooterDiv = document.querySelector(".kr-footer")
 
 
 dinnerNav.addEventListener(`click`, function(){
     document.body.classList.toggle(`dinnerBG`)
     phoneNumberDiv.classList.toggle(`dinnerBodyCopy`)
     reservationsDiv.classList.toggle(`dinnerBodyCopy`)
-    // bodyCopy.classList.toggle(`dinnerBodyCopy`)
+    krfooterDiv.classList.toggle(`dinnerBodyCopy`)
+    navLIs.forEach(function(navLi){
+        navLi.classList.toggle(`dinner-highlight-others`)
+    })
     dinnerNav.classList.toggle(`dinner-highlight`)
 })
