26e837d9a5ded14bfd012bbe70684e39adff9b42
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-08-29 16:45:59 -0400

    dried up the function

--- dist/js/gellijs.js
+++ dist/js/gellijs.js
@@ -1,79 +1,82 @@
-'use strict';
+"use strict";
 
 // window.addEventListener('resize', function(){
 var mql = window.matchMedia("(max-width: 640px)");
 
-var content = document.querySelector('.content');
-var dinner = document.querySelector('.dinner');
-// const dinnerDescription = document.querySelector('#dinnerDescription');
-var dinnerDescription = document.querySelector('.dinnerDescription');
-var dinnerPreview = document.querySelector('.panel');
-var dinnerDash = document.querySelector('.nav-dash.dinner');
-var dinnerH2 = document.querySelector('.dinnerH2');
-// var item3 = document.getElementById('item3');
-// var item4 = document.getElementById('item4');
+var menuItemNames = ["food", "bureau", "bar"];
 
-var small = {
-    setup: function setup() {
-        console.log("small setup");
-        dinnerH2.addEventListener('click', small.onClick);
-    },
-    teardown: function teardown() {
-        console.log("small teardown");
-        dinnerH2.removeEventListener('click', small.onClick);
-    },
-    onClick: function onClick() {
-        if (dinnerPreview.classList.contains("close")) {
-            dinnerPreview.classList.add("open");
-            dinnerPreview.classList.remove("close");
-        } else {
-            dinnerPreview.classList.add("close");
-            dinnerPreview.classList.remove("open");
+menuItemNames.forEach(function (menuItemName) {
+
+    var content = document.querySelector('.content');
+    var description = document.querySelector("." + menuItemName + "Description");
+    var preview = document.querySelector('.panel');
+    var h2 = document.querySelector("." + menuItemName + "H2"); // var item4 = document.getElementById('item4');
+
+    var small = {
+        setup: function setup() {
+            console.log("small setup");
+            h2.addEventListener('click', small.onClick);
+        },
+        teardown: function teardown() {
+            console.log("small teardown");
+            h2.removeEventListener('click', small.onClick);
+        },
+        onClick: function onClick() {
+            if (preview.classList.contains("close")) {
+                document.body.classList.add(menuItemName);
+                preview.classList.add("open");
+                preview.classList.remove("close");
+            } else {
+                preview.classList.add("close");
+                preview.classList.remove("open");
+                document.body.classList.remove(menuItemName);
+            }
+
+            description.classList.remove("open");
         }
+    };
 
-        dinnerDescription.classList.remove('open');
-    }
-};
+    var large = {
+        setup: function setup() {
+            console.log("large setup");
+            h2.addEventListener('click', large.onClick);
+        },
+        teardown: function teardown() {
+            console.log("large teardown");
+            h2.removeEventListener('click', large.onClick);
+        },
+        onClick: function onClick() {
+            console.log("not appending large");
+            h2.appendChild(description);
 
-var large = {
-    setup: function setup() {
-        console.log("large setup");
-        dinnerH2.addEventListener('click', large.onClick);
-    },
-    teardown: function teardown() {
-        console.log("large teardown");
-        dinnerH2.removeEventListener('click', large.onClick);
-    },
-    onClick: function onClick() {
-        console.log('not appending large');
-        dinnerH2.appendChild(dinnerDescription);
-
-        if (dinnerDescription.classList.contains("close")) {
-            dinnerDescription.classList.add("open");
-            dinnerDescription.classList.remove("close");
-        } else {
-            dinnerDescription.classList.add("close");
-            dinnerDescription.classList.remove("open");
+            if (description.classList.contains("close")) {
+                document.body.classList.add("food");
+                description.classList.add("open");
+                description.classList.remove("close");
+            } else {
+                description.classList.add("close");
+                document.body.classList.remove("food");
+                description.classList.remove("open");
+            }
         }
-    }
-};
+    };
 
-mql.addListener(function (data) {
-    if (data.matches) {
-        console.log("mql changed to small");
-        large.teardown();
+    mql.addListener(function (data) {
+        if (data.matches) {
+            console.log("mql changed to small");
+            large.teardown();
+            small.setup();
+        } else {
+            console.log("mql changed to large");
+            small.teardown();
+            large.setup();
+        }
+    });
+
+    if (mql.matches) {
         small.setup();
     } else {
-        console.log("mql changed to large");
-        small.teardown();
         large.setup();
     }
 });
-
-if (mql.matches) {
-    small.setup();
-} else {
-    large.setup();
-}
-// });
 //# sourceMappingURL=gellijs.js.map
--- src/js/gellijs.js
+++ src/js/gellijs.js
@@ -1,78 +1,90 @@
 // window.addEventListener('resize', function(){
 const mql = window.matchMedia("(max-width: 640px)");
 
-const content = document.querySelector('.content');
-const dinner = document.querySelector('.dinner');
-// const dinnerDescription = document.querySelector('#dinnerDescription');
-const dinnerDescription = document.querySelector('.dinnerDescription');
-const dinnerPreview = document.querySelector('.panel');
-const dinnerDash = document.querySelector('.nav-dash.dinner');
-const dinnerH2 = document.querySelector('.dinnerH2') ;
-// var item3 = document.getElementById('item3');
-// var item4 = document.getElementById('item4');
 
-let small = {
-    setup: function() {
-        console.log("small setup")
-        dinnerH2.addEventListener('click', small.onClick);
-    },
-    teardown: function() {
-        console.log("small teardown")
-        dinnerH2.removeEventListener('click', small.onClick);
-    },
-    onClick: function() { 
-        if (dinnerPreview.classList.contains("close")){
-            dinnerPreview.classList.add("open");
-            dinnerPreview.classList.remove("close");
-        } else {
-            dinnerPreview.classList.add("close");
-            dinnerPreview.classList.remove("open");
-        }
 
-        dinnerDescription.classList.remove(`open`)
-    }
-}
-    
-let large = {
-    setup: function() {
-        console.log("large setup")
-        dinnerH2.addEventListener('click', large.onClick)
-    },
-    teardown: function() {
-        console.log("large teardown")
-        dinnerH2.removeEventListener('click', large.onClick)
-    },
-    onClick: function() {
-        console.log(`not appending large`)
-        dinnerH2.appendChild(dinnerDescription)
+let menuItemNames = [
+    "food",
+    "bureau",
+    "bar"
+]
 
-        if (dinnerDescription.classList.contains("close")){
-            dinnerDescription.classList.add("open");
-            dinnerDescription.classList.remove("close");
-        } else {
-            dinnerDescription.classList.add("close");
-            dinnerDescription.classList.remove("open");
+
+menuItemNames.forEach(function(menuItemName) {
+        
+    const content = document.querySelector('.content');
+    const description = document.querySelector(`.${menuItemName}Description`);
+    const preview = document.querySelector('.panel');
+    const h2 = document.querySelector(`.${menuItemName}H2`) ;// var item4 = document.getElementById('item4');
+
+    let small = {
+        setup: function() {
+            console.log("small setup")
+            h2.addEventListener('click', small.onClick);
+        },
+        teardown: function() {
+            console.log("small teardown")
+            h2.removeEventListener('click', small.onClick);
+        },
+        onClick: function() { 
+            if (preview.classList.contains("close")){
+                document.body.classList.add(menuItemName)
+                preview.classList.add("open")
+                preview.classList.remove("close")
+            } else {
+                preview.classList.add("close")
+                preview.classList.remove("open")
+                document.body.classList.remove(menuItemName)
+            }
+
+            description.classList.remove(`open`)
         }
     }
-};
+        
+    let large = {
+        setup: function() {
+            console.log("large setup")
+            h2.addEventListener('click', large.onClick)
+        },
+        teardown: function() {
+            console.log("large teardown")
+            h2.removeEventListener('click', large.onClick)
+        },
+        onClick: function() {
+            console.log(`not appending large`)
+            h2.appendChild(description)
+
+            
+            if (description.classList.contains("close")){
+                document.body.classList.add(`food`)
+                description.classList.add("open")
+                description.classList.remove("close")
+            } else {
+                description.classList.add("close")
+                document.body.classList.remove(`food`)
+                description.classList.remove("open")
+            }
+        }
+    };
 
 
-mql.addListener(function(data) {
-    if (data.matches) {
-        console.log("mql changed to small")
-        large.teardown()
+    mql.addListener(function(data) {
+        if (data.matches) {
+            console.log("mql changed to small")
+            large.teardown()
+            small.setup()
+        } else {
+            console.log("mql changed to large")
+            small.teardown()
+            large.setup()
+        }
+    })
+
+    if (mql.matches) {
         small.setup()
+
     } else {
-        console.log("mql changed to large")
-        small.teardown()
         large.setup()
-    }
+    }   
+    
 })
-
-if (mql.matches) {
-    small.setup()
-
-} else {
-    large.setup()
-}   
-// });
