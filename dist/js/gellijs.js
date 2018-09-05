424bb3eab7ba2119c0b4ad0d1d6e17a55048ec6b
Ashley <ash.pollard11@yahoo.com>
2018-09-05 14:29:53 -0400

    readded gellijs.js

--- dist/js/gellijs.js
+++ dist/js/gellijs.js
@@ -1,29 +1,145 @@
 "use strict";
 
-var dinnerNav = document.querySelector(".dinner");
-var navLIs = document.querySelectorAll("nav a");
-var phoneNumberDiv = document.querySelector(".phone-number");
-var reservationsDiv = document.querySelector(".reservations");
-var krfooterDiv = document.querySelector(".kr-footer");
+var mql = window.matchMedia("(max-width: 640px)");
 
-dinnerNav.addEventListener("click", function () {
-    document.body.classList.toggle("dinnerBG");
-    phoneNumberDiv.classList.toggle("dinnerBodyCopy");
-    reservationsDiv.classList.toggle("dinner-highlight");
-    krfooterDiv.classList.toggle("dinnerBodyCopy");
-    var panel = document.querySelector(".panel");
+var menuItemNames = ["food", "bureau", "bar", "table", "location", "hours"];
 
-    navLIs.forEach(function (navLi) {
-        navLi.classList.toggle("dinner-highlight-others");
+var allDescriptions = document.querySelectorAll(".descriptionDiv");
+
+menuItemNames.forEach(function (menuItemName) {
+
+    var theLI = document.querySelector("li." + menuItemName);
+    var h2 = theLI.querySelector("h2");
+    var description = document.querySelector("." + menuItemName + "Description");
+    var preview = document.querySelector(".preview");
+
+    var small = {
+        setup: function setup() {
+            console.log("small setup");
+            h2.addEventListener("click", small.onClick);
+        },
+        teardown: function teardown() {
+            console.log("small teardown");
+            h2.removeEventListener("click", small.onClick);
+        },
+        onClick: function onClick() {
+            h2.appendChild(description);
+            description.classList.add("open");
+            small.clearSpecialClassOnBody();
+
+            if (!h2.classList.contains("open")) {
+                h2.classList.add("open");
+                document.body.classList.add(menuItemName);
+                description.classList.add("open");
+                console.log("open");
+            } else {
+                h2.classList.remove("open");
+                description.classList.remove("open");
+                console.log("close");
+            }
+        },
+
+        clearSpecialClassOnBody: function clearSpecialClassOnBody() {
+            menuItemNames.forEach(function (itemName) {
+                document.body.classList.remove(itemName);
+            });
+        },
+        onlyShowOnePanel: function onlyShowOnePanel(e) {
+            allDescriptions.forEach(function (el) {
+                el.classList.remove("open");
+            });
+            console.log(description);
+        }
+    };
+
+    var large = {
+        setup: function setup() {
+            console.log("large setup");
+            theLI.addEventListener('mouseover', large.hover);
+            theLI.addEventListener('mouseout', large.offHover);
+            h2.addEventListener('click', large.onClick);
+        },
+        teardown: function teardown() {
+            console.log("large teardown");
+            theLI.removeEventListener('mouseover', large.hover);
+            theLI.removeEventListener('mouseout', large.offHover);
+            h2.removeEventListener('click', large.onClick);
+        },
+        onClick: function onClick() {
+            h2.appendChild(description);
+            description.classList.add("open");
+            large.clearSpecialClassOnBody();
+
+            if (!h2.classList.contains("open")) {
+                h2.classList.add("open");
+                document.body.classList.add(menuItemName);
+                description.classList.add("open");
+                console.log("open");
+                preview.style.display = "none";
+            } else {
+                h2.classList.remove("open");
+                description.classList.remove("open");
+                console.log("close");
+                preview.removeAttribute("style");
+            }
+        },
+
+        clearSpecialClassOnBody: function clearSpecialClassOnBody() {
+            menuItemNames.forEach(function (itemName) {
+                document.body.classList.remove(itemName);
+            });
+        },
+        onlyShowOnePanel: function onlyShowOnePanel(e) {
+            allDescriptions.forEach(function (el) {
+                el.classList.remove("open");
+            });
+            console.log(description);
+        },
+
+        hover: function hover() {
+            document.body.classList.add(menuItemName);
+        },
+        offHover: function offHover() {
+            document.body.classList.remove(menuItemName);
+            preview.removeAttribute("style");
+            description.classList.remove("open");
+            h2.classList.remove("open");
+        }
+    };
+
+    var onClick = function onClick() {
+        h2.appendChild(description);
+        description.classList.add("open");
+        small.clearSpecialClassOnBody();
+
+        if (!h2.classList.contains("open")) {
+            h2.classList.add("open");
+            document.body.classList.add(menuItemName);
+            description.classList.add("open");
+            console.log("open");
+        } else {
+            h2.classList.remove("open");
+            description.classList.remove("open");
+            console.log("close");
+        }
+    };
+
+    mql.addListener(function (data) {
+        if (data.matches) {
+            console.log("mql changed to small");
+            large.teardown();
+            small.setup();
+        } else {
+            console.log("mql changed to large");
+            small.teardown();
+            large.setup();
+        }
     });
-    dinnerNav.classList.toggle("dinner-highlight");
 
-    if (panel.classList.contains("close")) {
-        panel.classList.add("open");
-        panel.classList.remove("close");
+    if (mql.matches) {
+        small.setup();
     } else {
-        panel.classList.add("close");
-        panel.classList.remove("add");
+        large.setup();
     }
 });
 //# sourceMappingURL=gellijs.js.map
--- src/js/gellijs.js
+++ src/js/gellijs.js
@@ -1,27 +1,161 @@
-let dinnerNav = document.querySelector(`.dinner`)
-let navLIs = document.querySelectorAll(`nav a`)
-let phoneNumberDiv = document.querySelector(".phone-number")
-let reservationsDiv = document.querySelector(".reservations")
-let krfooterDiv = document.querySelector(".kr-footer")
+
+const mql = window.matchMedia(`(max-width: 640px)`);
 
 
-dinnerNav.addEventListener(`click`, function(){
-    document.body.classList.toggle(`dinnerBG`)
-    phoneNumberDiv.classList.toggle(`dinnerBodyCopy`)
-    reservationsDiv.classList.toggle(`dinner-highlight`)
-    krfooterDiv.classList.toggle(`dinnerBodyCopy`)
-    const panel = document.querySelector(".panel")
 
-    navLIs.forEach(function(navLi){
-        navLi.classList.toggle(`dinner-highlight-others`)
-    })
-    dinnerNav.classList.toggle(`dinner-highlight`)
+let menuItemNames = [
+    `food`,
+    `bureau`,
+    `bar`,
+    `table`,
+    `location`,
+    `hours`
+]
 
-    if (panel.classList.contains("close")){
-        panel.classList.add("open");
-        panel.classList.remove("close");
-    } else {
-        panel.classList.add("close");
-        panel.classList.remove("add");
+
+const allDescriptions =  document.querySelectorAll(".descriptionDiv");
+
+menuItemNames.forEach(function(menuItemName) {
+    
+    const theLI = document.querySelector(`li.${menuItemName}`);
+    const h2 = theLI.querySelector(`h2`)
+    const description = document.querySelector(`.${menuItemName}Description`)
+    const preview = document.querySelector(`.preview`)
+
+    let small = {
+        setup: function() {
+            console.log(`small setup`)
+            h2.addEventListener(`click`, small.onClick);
+        },
+        teardown: function() {
+            console.log("small teardown")
+            h2.removeEventListener(`click`, small.onClick);
+        },
+        onClick: function() { 
+            h2.appendChild(description)
+            description.classList.add("open");
+            small.clearSpecialClassOnBody()
+            
+            if (!h2.classList.contains(`open`)){ 
+                h2.classList.add(`open`);
+                document.body.classList.add(menuItemName)
+                description.classList.add(`open`)
+                console.log(`open`)
+            }  else {
+                h2.classList.remove(`open`);
+                description.classList.remove(`open`)
+                console.log(`close`)
+            }
+    
+        },
+
+        clearSpecialClassOnBody: function() {
+            menuItemNames.forEach((itemName) => {
+                document.body.classList.remove(itemName)
+            })
+        },
+        onlyShowOnePanel: function(e){
+            allDescriptions.forEach(function(el) {
+                el.classList.remove("open");
+            });
+            console.log(description)
+        }
     }
+        
+
+
+    let large = {
+        setup: function() {
+            console.log(`large setup`)
+            theLI.addEventListener('mouseover', large.hover)
+            theLI.addEventListener('mouseout', large.offHover)
+            h2.addEventListener('click', large.onClick)
+        },
+        teardown: function() {
+            console.log(`large teardown`)
+            theLI.removeEventListener('mouseover', large.hover)
+            theLI.removeEventListener('mouseout', large.offHover)
+            h2.removeEventListener('click', large.onClick)
+            
+        },
+        onClick: function() { 
+            h2.appendChild(description)
+            description.classList.add("open");
+            large.clearSpecialClassOnBody()
+            
+            if (!h2.classList.contains(`open`)){ 
+                h2.classList.add(`open`);
+                document.body.classList.add(menuItemName)
+                description.classList.add(`open`)
+                console.log(`open`)
+                preview.style.display = "none"
+            }  else {
+                h2.classList.remove(`open`);
+                description.classList.remove(`open`)
+                console.log(`close`)
+                preview.removeAttribute(`style`)
+            }
+    
+        },
+
+        clearSpecialClassOnBody: function() {
+            menuItemNames.forEach((itemName) => {
+                document.body.classList.remove(itemName)
+            })
+        },
+        onlyShowOnePanel: function(e){
+            allDescriptions.forEach(function(el) {
+                el.classList.remove("open");
+            });
+            console.log(description)
+        },
+
+        hover: function(){
+            document.body.classList.add(menuItemName)
+        },
+        offHover: function(){
+            document.body.classList.remove(menuItemName)
+            preview.removeAttribute(`style`)
+            description.classList.remove(`open`)
+            h2.classList.remove(`open`);
+        }
+    }
+
+    let onClick = function(){
+        h2.appendChild(description)
+        description.classList.add("open");
+        small.clearSpecialClassOnBody()
+        
+        if (!h2.classList.contains(`open`)){ 
+            h2.classList.add(`open`);
+            document.body.classList.add(menuItemName)
+            description.classList.add(`open`)
+            console.log(`open`)
+        }  else {
+            h2.classList.remove(`open`);
+            description.classList.remove(`open`)
+            console.log(`close`)
+        }
+    }
+
+
+    mql.addListener(function(data) {
+        if (data.matches) {
+            console.log(`mql changed to small`)
+            large.teardown()
+            small.setup()
+        } else {
+            console.log(`mql changed to large`)
+            small.teardown()
+            large.setup()
+        }
+    })
+
+    if (mql.matches) {
+        small.setup()
+
+    } else {
+        large.setup()
+    }   
+    
 })
